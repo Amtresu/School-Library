f60ff8433e59de255988a9d4d7a6ababf27f130c
Amtresu <conor.follette@gmail.com>
2022-11-08 20:28:19 +0900

    fixed linter errors

--- modules/mod_rentals.rb
+++ modules/mod_rentals.rb
@@ -6,38 +6,37 @@ require_relative './local_storage'
 module ModRentals
   include LocalStorage
 
-    def space_checker(books, people, rentals)
-      if books.size.zero?
-        puts 'No Books Available to Rent'
-      elsif people.size.zero?
-        puts 'No People Available to Rent to'
-      else
-        create_rental(books, people, rentals)
-      end
-      menu_return
+  def space_checker(books, people, rentals)
+    if books.size.zero?
+      puts 'No Books Available to Rent'
+    elsif people.size.zero?
+      puts 'No People Available to Rent to'
+    else
+      create_rental(books, people, rentals)
     end
+    menu_return
+  end
 
-    def create_rental(books, people, rentals)
-      rental_storage = fetch_storage('rentals')
-      puts 'Select a book to rent'
-      books.each_with_index { |book, index| puts "#{index}) Book Title: #{book.title}, Author: #{book.author}" }
-      rental_book = gets.chomp.to_i
-      puts 'Select Person by their ID'
-      people.each_with_index do |person, index|
-        puts "#{index}) #{[person.type]} Name: #{person.name} Age: #{person.age} Id: #{person.id}"
-      end
-      rental_person = gets.chomp.to_i
-      puts 'Enter date of Rental'
-      date = gets.chomp
-      rental_item = Rental.new(people[rental_person], books[rental_book], date)
-      rental_data = { date: date, book_index: rental_book, person_index: rental_person }
-      rentals.push(rental_item)
-      rental_storage.push(rental_data)
-      update_storage('rentals', rental_storage)
-      puts 'Rental Successfully Created'
+  def create_rental(books, people, rentals)
+    rental_storage = fetch_storage('rentals')
+    puts 'Select a book to rent'
+    books.each_with_index { |book, index| puts "#{index}) Book Title: #{book.title}, Author: #{book.author}" }
+    rental_book = gets.chomp.to_i
+    puts 'Select Person by their ID'
+    people.each_with_index do |person, index|
+      puts "#{index}) #{[person.type]} Name: #{person.name} Age: #{person.age} Id: #{person.id}"
     end
-  
-  
+    rental_person = gets.chomp.to_i
+    puts 'Enter date of Rental'
+    date = gets.chomp
+    rental_item = Rental.new(people[rental_person], books[rental_book], date)
+    rental_data = { date: date, book_index: rental_book, person_index: rental_person }
+    rentals.push(rental_item)
+    rental_storage.push(rental_data)
+    update_storage('rentals', rental_storage)
+    puts 'Rental Successfully Created'
+  end
+
   def display_rentals(rentals, people)
     puts 'Select id of any person'
     people.each { |i| puts "[#{i.type.to_i}] id: #{i.id}, Person: #{i.name}" }
